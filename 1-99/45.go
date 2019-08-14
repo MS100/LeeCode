4988dcc589f9917de6ffc0ea860c095e5841e519
ms <ms1.0@qq.com>
2019-08-14 10:57:34 +0800

    update

---
+++ 1-99/45.go
@@ -0,0 +1,60 @@
+func jump2(nums []int) int {
+    l := len(nums)-1
+    if l == 0 {
+        return 0
+    }
+    
+    if nums[0] >= l {
+        return 1
+    }
+    
+    x,max,n := 2,nums[0],nums[0]
+    
+    for i:= 1;i<l;i++{
+        t := i+nums[i]
+        if t > max {
+            max = t
+            if max >= l {
+                return x
+            }
+        }
+        
+        if i == n {
+            x++
+            n = max
+        }
+    }
+        
+    
+    return x
+}
+
+func jump(nums []int) int {
+    l := len(nums)-1
+    if l == 0 {
+        return 0
+    }
+    
+    if nums[0] >= l {
+        return 1
+    }
+    
+    x,max,m,n := 2,nums[0],1,nums[0]
+    
+    for {
+        for i:= m;i<=n;i++{
+            t := i+nums[i]
+            if t > max {
+                max = t
+                if max >= l {
+                    return x
+                }
+            }
+        }
+        
+        m,n = n+1,max
+        x++
+    }
+    
+    return x
+}
